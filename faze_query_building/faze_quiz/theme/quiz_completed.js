c6cf3764816fb0cddffc57fbb08279bafd52e3b1
gagi <gagi.shmagi@gmail.com>
2014-10-21 13:39:34 +0300

    removed duplicate folders

--- faze_query_building/faze_quiz/theme/quiz_completed.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $(".question .explanation_link>a").click(function() {
-    $(this).parents(".question").find(".explanation").toggle(300);
-    return false;
-  }); 
-});
-
